4603b42d8390d56d74c7e5d15d1642320efe14f8
Gabriella <kovacsxgabriella@gmail.com>
2017-12-10 21:41:53 +0100

    Add live data layout and style

---
+++ src/js/infoPanel.js
@@ -0,0 +1,20 @@
+(function(){
+
+	var timeJakarta = document.querySelector('.js-time-jakarta');
+	var timeBrussels = document.querySelector('.js-time-brussels');
+
+	var temperatureJakarta = document.querySelector('.js-temperature-jakarta');
+	var temperatureBrussels = document.querySelector('.js-temperature-brussels');
+
+	var humidityJakarta = document.querySelector('.js-humidity-jakarta');
+	var humidityBrussels = document.querySelector('.js-humidity-brussels');
+
+	timeJakarta.innerHTML = '19:45';
+	timeBrussels.innerHTML = '12:45';
+
+	temperatureJakarta.innerHTML = '11';
+	temperatureBrussels.innerHTML = '20';
+
+	humidityJakarta.innerHTML = '40%';
+	humidityBrussels.innerHTML = '55%';
+}())
