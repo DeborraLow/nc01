8a5a31478ef09126d691f8d53f4ec6cac754f2d3
Gabriella <kovacsxgabriella@gmail.com>
2017-12-10 21:43:47 +0100

    Add interactivity to 3d model

--- src/js/modelInteraction.js
+++ src/js/modelInteraction.js
@@ -1,4 +1,33 @@
-var tween = TweenMax.to("#js-animation", 1.0, {
-    backgroundPosition: "0 100%", 
-    ease: SteppedEase.config(31)
-})
+(function(){
+
+	var modelInteractionArea = document.querySelector('.js-model-interaction-area');
+	var model = document.querySelector('.js-3d-model');
+	var modelInteractionAreaWidth = modelInteractionArea.getBoundingClientRect().width;
+	var nrOfSegments = 90; //minus one
+
+	modelInteractionArea.addEventListener('mousemove', function(e) {
+		modelRotateANimation(e.clientX);
+	})
+
+	function modelRotateANimation(mousePosX) {
+
+		var ratio = Math.round(mousePosX / modelInteractionAreaWidth * nrOfSegments) / nrOfSegments * 100;
+
+		console.log('rounding ' + Math.round(mousePosX / modelInteractionAreaWidth * nrOfSegments));
+
+		console.log('ratio ' + ratio);
+
+		return (
+			TweenMax.to('.js-3d-model', 0.01, 
+			{
+			    backgroundPosition: '0 ' + ratio + '%', 
+			    ease: SteppedEase.config(1)
+			})
+		)
+
+	}
+
+
+}())
+
+
